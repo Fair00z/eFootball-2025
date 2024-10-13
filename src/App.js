9c2037125a110e4c72caca5d7f6e6c4c9c78bedf
Fairooz <hiiamfairooz@gmail.com>
2024-10-13 15:06:02 +0530

    top header setted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import TopHeader from "./Components/top_header/top_header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopHeader/>
     </div>
   );
 }
---
+++ src/Components/top_header/top_header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import gp from '../../Assets/images/gp.png'
+import coin from '../../Assets/images/efootball-coins.webp'
+import './top_header.css'
+
+function TopHeader() {
+    return(
+        <div>
+            <nav>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="d-flex justify-content-around nav-top mt-1 px-5">
+                            <div className="d-flex">
+                                <div className="ratio ratio-1x1" style={{width: '37px', height: '37px'}}>
+                                    <img style={{width: '100%', height: '100%', objectfit: 'cover',}} src={coin}></img>
+                                </div>
+                                <div className="pt-2">
+                                    <p className="text-white ms-2">12,000</p>
+                                </div>
+                            </div>
+                            <div className="d-flex">
+                                <div className="ratio ratio-1x1" style={{width: '37px', height: '37px',}}>
+                                    <img style={{width: '100%', height: '100%', objectfit: 'cover',}} src={gp} alt=""></img>
+                                </div>
+                                <div className="pt-2">
+                                    <p className="text-white ms-2">15,23000</p>
+                                </div>
+                            </div>
+                            
+                            <i className="fa-solid fa-circle-info top-nav-icons"></i>
+                            <i className="fa-solid fa-user-group top-nav-icons"></i>
+                            <i className="fa-solid fa-envelope top-nav-icons"></i>
+                            <i className="fa-solid fa-bell top-nav-icons"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        </div>
+    )
+    
+}
+export default TopHeader;
